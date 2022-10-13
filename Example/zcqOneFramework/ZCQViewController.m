6a7ea31d2d634fdb01925c70b9f53f2c46ae9aec
Z cq <494313298@qq.com>
2022-10-13 16:25:01 +0800

    提交新版本

---
+++ Example/zcqOneFramework/ZCQViewController.m
@@ -0,0 +1,28 @@
+//
+//  ZCQViewController.m
+//  zcqOneFramework
+//
+//  Created by Z cq on 10/13/2022.
+//  Copyright (c) 2022 Z cq. All rights reserved.
+//
+
+#import "ZCQViewController.h"
+@interface ZCQViewController ()
+
+@end
+
+@implementation ZCQViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ zcqOneFramework/Classes/DemoVender.h
@@ -0,0 +1,19 @@
+//
+//  DemoVender.h
+//  Pods
+//
+//  Created by 郑创权 on 2022/10/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DemoVender : UIView
+
+
+-(void)mxsss;
+
+@end
+
+NS_ASSUME_NONNULL_END
